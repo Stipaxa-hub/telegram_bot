5660e2623e930d5ee0cb4f8454890c373b28f616
Stepan <petrivs572@gmail.com>
2024-03-14 18:27:25 +0200

    Fixed comments

--- src/test/java/org/project/bot/service/impl/DataCalculateServiceImplTest.java
+++ src/test/java/org/project/bot/service/impl/DataCalculateServiceImplTest.java
@@ -1,7 +1,6 @@
 package org.project.bot.service.impl;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -9,7 +8,6 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
-import java.time.Month;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,45 +16,14 @@ class DataCalculateServiceImplTest {
     @InjectMocks
     private DataCalculateServiceImpl dataCalculateService;
 
-    @Test
-    @DisplayName("Calculate percent for the first day in year")
-    void calculatePercent_firstDayInYear() {
-        LocalDate firstYearDay = LocalDate.of(2024, Month.JANUARY, 1);
-
-        String expected = "0.27";
-        String actual = dataCalculateService.calculatePercent(firstYearDay);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Calculate percent for the last day in year")
-    void calculatePercent_lastDayInYear() {
-        LocalDate lastYearDay = LocalDate.of(2024, Month.DECEMBER, 31);
-
-        String expected = "100.00";
-        String actual = dataCalculateService.calculatePercent(lastYearDay);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Calculate percent for the middle day in year")
-    void calculatePercent_middleDayInYear() {
-        LocalDate middleYearDay = LocalDate.of(2024, Month.JULY, 1);
-
-        String expected = "50.00";
-        String actual = dataCalculateService.calculatePercent(middleYearDay);
-
-        assertEquals(expected, actual);
-    }
-
     @ParameterizedTest
     @CsvSource(
             {
                     "2024-01-01, 0.27",
                     "2024-05-05, 34.43",
-                    "2024-10-28, 82.51"
+                    "2024-10-28, 82.51",
+                    "2024-07-01, 50.00",
+                    "2024-12-31, 100.00"
             }
     )
     @DisplayName("Calculate percent for the random day in year")
